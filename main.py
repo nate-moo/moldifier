575027f64bd3413425e267ca51ef79ce86a34a08
nate-moo <40650681+nate-moo@users.noreply.github.com>
2021-09-20 11:50:54 -0400

    added an iteration mode

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt, QObject, QRunnable, pyqtSlot, QThreadPool, QTimer
+from PyQt5.QtWidgets import QApplication
+
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
 class Ui_MainWindow(object):
 
@@ -11,7 +16,7 @@ class Ui_MainWindow(object):
         self.pushButton.setGeometry(QtCore.QRect(90, 30, 75, 23))
         self.pushButton.setObjectName('pushButton')
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(90, 130, 75, 23))
+        self.pushButton_2.setGeometry(QtCore.QRect(90, 160, 75, 23))
         self.pushButton_2.setObjectName('pushButton_2')
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit.setGeometry(QtCore.QRect(70, 90, 113, 21))
@@ -19,36 +24,39 @@ class Ui_MainWindow(object):
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(100, 70, 47, 13))
         self.label.setObjectName('label')
+        self.label1 = QtWidgets.QLabel(self.centralwidget)
+        self.label1.setGeometry(QtCore.QRect(103, 120, 113, 13))
+        self.label2 = QtWidgets.QLabel(self.centralwidget)
+        self.label2.setGeometry(QtCore.QRect(190, 130, 113, 13))
+        self.slider = QtWidgets.QSlider(self.centralwidget)
+        self.slider.setGeometry(QtCore.QRect(66, 130, 120, 22))
+        self.slider.setOrientation(QtCore.Qt.Horizontal)
+        self.slider.setValue(0)
+        self.slider.setMaximum(20)
+        self.slider.valueChanged.connect(self.setText)
+
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 268, 21))
-        self.menubar.setObjectName("menubar")
-        self.menuFile = QtWidgets.QMenu(self.menubar)
-        self.menuFile.setObjectName("menuFile")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.actionSize_Targeter = QtWidgets.QAction(MainWindow)
-        self.actionSize_Targeter.setObjectName("actionSize_Targeter")
-        self.menuFile.addAction(self.actionSize_Targeter)
-        self.menubar.addAction(self.menuFile.menuAction())
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(90, 150, 75, 23))
-        self.pushButton_3.setObjectName('pushButton_3')
-        MainWindow.setStatusBar(self.statusbar)
+        # self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        # self.pushButton_3.setGeometry(QtCore.QRect(90, 150, 75, 23))
+        # self.pushButton_3.setObjectName('pushButton_3')
+        
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
 
+    def setText(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.label2.setText(str(self.slider.value()))
+        return
+        
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate('MainWindow', 'Moldifier'))
         self.pushButton.setText(_translate('MainWindow', 'Open File'))
         self.pushButton_2.setText(_translate('MainWindow', 'Compress'))
+        self.label1.setText(_translate("MainWindow", "Iterations"))
         self.label.setText(_translate('MainWindow', 'Filename'))
-        self.menuFile.setTitle(_translate("MainWindow", "File"))
-        self.actionSize_Targeter.setText(_translate("MainWindow", "Size Targeter"))
+
 
 
 if __name__ == '__main__':
--- moldifier.py
+++ moldifier.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import QObject, QThread, pyqtSignal, Qt
 from PyQt5.QtWidgets import QFileDialog, QApplication
+from tempfile import TemporaryDirectory
 from main import Ui_MainWindow
 import sys
 import os
@@ -21,11 +22,11 @@ class mainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.btnPushButton)
         self.ui.pushButton_2.clicked.connect(self.btnPushButton2)
-        self.ui.actionSize_Targeter.clicked.connect(self.)
+        # self.ui.actionSize_Targeter.clicked.connect(self.)
         self.filePath = ""
     
-    def btnSpawnNewWindow(self):
-        return
+    # def btnSpawnNewWindow(self):
+    #     return
 
     def btnPushButton(self):
         print("hello")
@@ -34,11 +35,25 @@ class mainWindow(QtWidgets.QMainWindow):
         self.filePath = dialog[0]
     
     def btnPushButton2(self):
-        print("goodbye")
         path = os.path.split(f"{self.filePath}")[0]
-        print(f"{self.filePath}")
-        print(f"{path}/{self.ui.lineEdit.text()}")
-        Popen([resource_path("ffmpeg.exe"), "-i", self.filePath, "-b:v", "10k", "-b:a", "5k", f"{path}/{self.ui.lineEdit.text()}"], shell=True)
+        if self.ui.slider.value() != 0:
+            with TemporaryDirectory(dir=path) as tempDir:
+                # tempDir = os.path.realpath(tempDir)
+                print(f"Temp Dir: {tempDir}")
+                t = 0
+                for a in range(self.ui.slider.value()):
+                    print(a)
+                    if a == 0:
+                        Popen([resource_path("ffmpeg.exe"), "-i", self.filePath, "-b:v", "20k", "-b:a", "10k", "-preset", "ultrafast", f"{tempDir}/{a+1}-{self.ui.lineEdit.text()}"], shell=True).wait()
+                    else:
+                        t = a
+                        Popen([resource_path("ffmpeg.exe"), "-i", f"{tempDir}/{a}-{self.ui.lineEdit.text()}", "-b:v", "20k", "-b:a", "10k", "-preset", "ultrafast", f"{tempDir}/{a+1}-{self.ui.lineEdit.text()}"], shell=True).wait()
+                os.rename(f"{tempDir}/{t}-{self.ui.lineEdit.text()}", f"{path}/{self.ui.lineEdit.text()}")
+        else:
+            print("goodbye")
+            print(f"{self.filePath}")
+            print(f"{path}/{self.ui.lineEdit.text()}")
+            Popen([resource_path("ffmpeg.exe"), "-i", self.filePath, "-b:v", "10k", "-b:a", "5k", "-preset", "ultrafast", f"{path}/{self.ui.lineEdit.text()}"], shell=True).wait()
         
     
 
